f96ea0d22a5ad0e28a0010b8801886a45ac3f47f
Eduardo Felipe <edufslima9@gmail.com>
2022-06-19 14:35:24 -0300

    feat(Program): adding ReadUsers functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+  class Program
+  {
+    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False";
+
+    static void Main(string[] args)
+    {
+      ReadUsers();
+    }
+
+    public static void ReadUsers()
+    {
+      using(var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var users = connection.GetAll<User>();
+
+        foreach (var user in users)
+        {
+          Console.WriteLine(user.Name);
+        }
+      }
+    }
+  }
+}
