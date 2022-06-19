1726719443496fcaa282170f4460e55530de4981
Eduardo Felipe <edufslima9@gmail.com>
2022-06-19 14:41:48 -0300

    feat(Program): adding CRUD User functionality

--- Program.cs
+++ Program.cs
@@ -11,7 +11,11 @@ namespace Blog
 
     static void Main(string[] args)
     {
-      ReadUsers();
+      //ReadUsers();
+      //ReadUser();
+      //CreateUser();
+      //UpdateUser();
+      DeleteUser();
     }
 
     public static void ReadUsers()
@@ -24,6 +28,66 @@ namespace Blog
         {
           Console.WriteLine(user.Name);
         }
+      }
+    }
+
+    public static void ReadUser()
+    {
+      using(var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var user = connection.Get<User>(1);
+
+        Console.WriteLine(user.Name);
+      }
+    }
+
+    public static void CreateUser()
+    {
+      var user = new User()
+      {
+        Bio = "Equipe balta.io",
+        Email = "hello@balta.io",
+        Image = "https://...",
+        Name = "Equipe balta.io",
+        PasswordHash = "HASH",
+        Slug = "equipe-balta"
+      };
+
+      using(var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        connection.Insert<User>(user);
+        Console.WriteLine("Cadastro realizado com sucesso!");
+      }
+    }
+
+    public static void UpdateUser()
+    {
+      var user = new User()
+      {
+        Id = 2,
+        Bio = "Equipe | balta.io",
+        Email = "hello@balta.io",
+        Image = "https://...",
+        Name = "Equipe de suporte balta.io",
+        PasswordHash = "HASH",
+        Slug = "equipe-balta"
+      };
+
+      using(var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        connection.Update<User>(user);
+        Console.WriteLine("Atualização realizada com sucesso!");
+      }
+    }
+
+    public static void DeleteUser()
+    {
+      using(var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var user = connection.Get<User>(2);
+
+        connection.Delete<User>(user);
+        Console.WriteLine("Exclusão realizada com sucesso!");
       }
     }
   }
